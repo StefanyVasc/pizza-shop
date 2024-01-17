0d1299013da9cab14ba8869d3201428fa051e8da
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-16 21:57:13 -0300

    feat: create charts

--- src/pages/app/dashboard/revenue-charts.tsx
+++ src/pages/app/dashboard/revenue-charts.tsx
@@ -54,6 +54,8 @@ export function RevenueChart() {
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               stroke={colors.violet[500]}
               type="linear"
