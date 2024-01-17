33061c05bcd7fd759ad35212c24cb3b64fd7b62f
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-17 18:27:43 -0300

    feat: setup api client

---
+++ src/lib/axios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+import { env } from '@/env'
+
+// api client -> client de API no front para se conectar com o backend
+
+export const api = axios.create({
+  baseURL: env.VITE_API_URL,
+})
