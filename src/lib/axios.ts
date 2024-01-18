5295ac8d21e24d4a8ee06271827a4b63c5de8c18
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-18 11:07:31 -0300

    feat(account): add skeleton when data isLoading

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -8,3 +8,12 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+// add delay. Os interceptors interceptam cada requisição
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
