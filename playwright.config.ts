1e2b0aed49b2d7c72eb165c4afd066dd1d2127fe
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-06-20 18:09:25 -0300

    tests: problems with sign-in in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test',
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   use: {
     baseURL: 'http://127.0.0.1:1234',
-    ignoreHTTPSErrors: true,
   },
   webServer: {
     command: 'npm run dev:test',
